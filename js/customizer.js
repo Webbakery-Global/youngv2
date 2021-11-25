fd1666ae95ea1447b6bc3eb0c4e4c1edbf1b458c
caspervanschooten <59822702+caspervanschooten@users.noreply.github.com>
2021-11-25 11:16:09 +0100

    Add files via upload

---
+++ js/customizer.js
@@ -0,0 +1,60 @@
+/**
+ * File customizer.js.
+ *
+ * Theme Customizer enhancements for a better user experience.
+ *
+ * Contains handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+
+( function( $ ) {
+
+	// Site title and description.
+	wp.customize( 'blogname', function( value ) {
+		value.bind( function( to ) {
+			$( '.site-title a' ).text( to );
+		} );
+	} );
+	wp.customize( 'blogdescription', function( value ) {
+		value.bind( function( to ) {
+			$( '.site-description' ).text( to );
+		} );
+	} );
+
+	// Header text color.
+	wp.customize( 'header_textcolor', function( value ) {
+		value.bind( function( to ) {
+			if ( 'blank' === to ) {
+				$( '.site-title a, .site-description' ).css( {
+					'clip': 'rect(1px, 1px, 1px, 1px)',
+					'position': 'absolute'
+				} );
+			} else {
+				$( '.site-title a, .site-description' ).css( {
+					'clip': 'auto',
+					'position': 'relative'
+				} );
+				$( '.site-title a, .site-description' ).css( {
+					'color': to
+				} );
+			}
+		} );
+	} );
+} )( jQuery );
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("main-nav");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+	if (window.pageYOffset > sticky) {
+		navbar.classList.add("sticky");
+	} else {
+		navbar.classList.remove("sticky");
+	}
+}
---
+++ js/editor.js
@@ -0,0 +1,12 @@
+// Remove Gutenberg items
+// import { unregisterBlockType } from '@wordpress/blocks';
+// import domReady from '@wordpress/dom-ready';
+
+// domReady( function () {
+//     unregisterBlockType( 'core/verse' );
+//     unregisterBlockType( 'core/buttons' );
+//     unregisterBlockType( 'core/quote' );
+//     unregisterBlockType( 'core/pullquote' );
+//     unregisterBlockType( 'core/social-link' );
+// } );
+
