4b9285726292afe0f5cf1c1d687dd9232d20bf1b
Casper van Schooten <casper@webbakery.nl>
2021-11-25 13:01:21 +0100

    Single project

---
+++ single-projecten.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header();
+?>
+
+<div class="container single_project">
+    <div class="row">
+        <div class="col-sm-12">
+            <h1><?php the_title();?></h1>
+
+            <?php the_content(); ?>
+        </div>
+    </div>
+</div>
+
+<?php
+
+
+
+get_footer();
