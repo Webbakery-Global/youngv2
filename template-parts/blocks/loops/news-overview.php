2f35a7a457032c6fa3fd89421f707d55f4f5f4fb
Casper van Schooten <casper@webbakery.nl>
2021-12-13 09:21:37 +0100

    Nieuws overzicht

--- template-parts/blocks/loops/news-overview.php
+++ template-parts/blocks/loops/news-overview.php
@@ -30,10 +30,10 @@ if (!empty($block['align'])) {
             if (!empty($terms)) :
                 $term = array_shift($terms); ?>
 
-                <div class="col-sm-12 col-lg-6 post_img">
+                <div class="col-sm-12 col-lg-6 post_img mb-2">
                     <?php echo get_the_post_thumbnail($post->ID, 'post_thumbnail', array('class' => 'logo')) ?>
                 </div>
-                <div class="col-sm-12 col-lg-6 post_content">
+                <div class="col-sm-12 col-lg-6 post_content mb-5">
                     <div class="content">
                         <h5 class="subtitle"><?php echo $term->name; ?></h5>
                         <h3><?php the_title(); ?></h3>
