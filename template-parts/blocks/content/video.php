fd5d639d6c961e2599a6cbb297dfe49dc8fb762f
Casper van Schooten <casper@webbakery.nl>
2021-12-17 17:25:03 +0100

    Video fix

--- template-parts/blocks/content/video.php
+++ template-parts/blocks/content/video.php
@@ -12,7 +12,7 @@ if (!empty($block['align'])) {
 
 ?>
 <div class="<?php echo esc_attr($className); ?>">
-    <video class="half_video"  autoplay muted loop>
+    <video class="half_video" playsinline autoplay muted loop>
         <source src="<?php the_field('video_url');?>" type="video/mp4">
         Your browser does not support the video tag.
     </video>
